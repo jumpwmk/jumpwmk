d30e28a93134354df7bcb82198800f2a209d6931
kiatchaiwat <methus159357@gmail.com>
2017-01-17 14:07:09 +0700

    update

--- code/test.py
+++ code/test.py
@@ -1,13 +1,16 @@
-h1 = int(input())
-m1 = int(input())
-s1 = int(input())
-h2 = int(input())
-m2 = int(input())
-s2 = int(input())
-t1 = h1*3600 + m1*60 + s1
-t2 = h2*3600 + m2*60 + s2
-dt = t2 - t1
-dh = dt // 3600
-dm = (dt - (dh * 3600))//60
-ds = dt - dm
-print(str(dh)+":"+str(dm)+":"+str(ds))
+x = int(input())
+s = ""
+
+if x == 0:
+	s = "zero "
+elif x > 0:
+	s = "positive "
+else:
+	s = "negative "
+
+if x % 2 == 0:
+	s = s + "even"
+else:
+	s = s + "odd"
+
+print(s)
